cedea9a97dd1d19f87880d190d9415ca3a9c9d88
sigitdwisap <sigit.dsa3@yahoo.co.id>
2019-04-27 23:55:07 +0700

    add binary search tree

---
+++ BinarySearchTree/src/com/basicandlogic/binarysearchtree/BinarySearchTree.java
@@ -0,0 +1,81 @@
+package com.basicandlogic.binarysearchtree;
+
+// Java program to demonstrate insert operation in binary search tree
+public class BinarySearchTree {
+
+    /* Class containing left and right child of current node and key value*/
+    class Node {
+        int key;
+        Node left, right;
+
+        public Node(int item) {
+            key = item;
+            left = right = null;
+        }
+    }
+
+    // Root of BST 
+    Node root;
+
+    // Constructor 
+    BinarySearchTree() {
+        root = null;
+    }
+
+    // This method mainly calls insertRec() 
+    void insert(int key) {
+        root = insertRec(root, key);
+    }
+
+    /* A recursive function to insert a new key in BST */
+    Node insertRec(Node root, int key) {
+
+        /* If the tree is empty, return a new node */
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+
+        /* Otherwise, recur down the tree */
+        if (key < root.key)
+            root.left = insertRec(root.left, key);
+        else if (key > root.key)
+            root.right = insertRec(root.right, key);
+
+        /* return the (unchanged) node pointer */
+        return root;
+    }
+
+    // This method mainly calls InorderRec() 
+    void inorder()  {
+        System.out.println();
+        System.out.println("Inorder transversal of BST");
+        inorderRec(root);
+    }
+
+    // A utility function to do inorder traversal of BST 
+    void inorderRec(Node root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.println(root.key);
+            inorderRec(root.right);
+        }
+    }
+
+    // This code is contributed by Ankur Narain Verma
+
+    // A utility function to search a given key in BST
+    public Node search(Node root, int key)
+    {
+        // Base Cases: root is null or key is present at root
+        if (root==null || root.key==key)
+            return root;
+
+        // val is greater than root's key
+        if (root.key > key)
+            return search(root.left, key);
+
+        // val is less than root's key
+        return search(root.right, key);
+    }
+}
---
+++ BinarySearchTree/src/com/basicandlogic/binarysearchtree/MainClass.java
@@ -0,0 +1,30 @@
+package com.basicandlogic.binarysearchtree;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+
+        /* Let us create following BST
+              50
+           /     \
+          30      70
+         /  \    /  \
+       20   40  60   80 */
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
+
+        System.out.println("The key of root : " + (tree.search(tree.root, 50)).key);
+        System.out.println("The key of left root : " + ((tree.search(tree.root, 50)).left).key);
+        System.out.println("The key of right root : " + ((tree.search(tree.root, 50)).right).key);
+
+        // print inorder traversal of the BST
+        tree.inorder();
+    }
+}
+
