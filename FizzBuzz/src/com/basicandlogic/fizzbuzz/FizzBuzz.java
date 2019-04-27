f988c66f921a01b9131bcc451309fa97bdd91740
sigitdwisap <sigit.dsa3@yahoo.co.id>
2019-04-28 00:18:15 +0700

    add fizz buzz

---
+++ FizzBuzz/src/com/basicandlogic/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,22 @@
+package com.basicandlogic.fizzbuzz;
+
+public class FizzBuzz {
+
+    FizzBuzz(int inputNumber) {
+        fizzBuzz(inputNumber);
+    }
+
+    private void fizzBuzz(int number){
+        for(int i = 1; i <= number; i++) {
+            if (i%3 == 0 && i%5 == 0){
+                System.out.println("FizzBuzz");
+            } else if (i%3 == 0) {
+                System.out.println("Fizz");
+            } else if (i%5 == 0) {
+                System.out.println("Buzz");
+            }  else {
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ FizzBuzz/src/com/basicandlogic/fizzbuzz/MainClass.java
@@ -0,0 +1,9 @@
+package com.basicandlogic.fizzbuzz;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz(20);
+    }
+}
+
